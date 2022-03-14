4794bfe19326f5c6c1592f12439871503cc9a51a
Viacheslav <sadko.viacheslav@gmail.com>
2022-03-13 21:58:39 -0400

    First module

--- Bank management system/main.c
+++ Bank management system/main.c
@@ -1,4 +1,31 @@
 #include <stdio.h>
+
+struct{
+    char name[255];
+    int age;
+}add;
+
+void registration(){
+    FILE * ptr;
+    ptr = fopen("record.dat", "a+");
+    scanf("%i",&add.age);
+    scanf("%s", add.name);
+    fprintf(ptr, "%s %i \n", add.name, add.age);
+    fclose(ptr);
+}
+
 int main(){
+    int n=0;
+    printf("Hello, welcome to the Bank management system\nSelect your menu\n");
+    printf("Menu:\n1) New client registration\n2) Exit\n");
+  
+        scanf("%d", &n);
+        switch(n){
+            case 1:
+            registration();
+            break;
+            case 2:
+            break;
+        }
     
 }
